b1ef82dc4837acb84a5060b47b7656ad41edf64c
Sepand Ansari <sepans@sepans.com>
2018-02-04 10:09:10 -0500

    ....

--- src/js/artsy.js
+++ src/js/artsy.js
@@ -29,38 +29,44 @@ function dragged(d) {
   d.y = Math.round(d3.event.y / stepY) * stepY + positionDiff[1];
   const trans = translate(d);
   d3.select(this).style("transform", trans);
-  //.attr('data-position',  )
 }
 
 function dragend() {
   console.log(this.getBoundingClientRect(), hintPos, pieceSize);
-  const dropPos = this.getBoundingClientRect();
-  console.log(
-    Math.round(dropPos.x / pieceSize[0]) -2,
-    Math.round(dropPos.y / pieceSize[1] - 1)
-  );
-  // const pos = d.y/stepY * puzzlePieces[0] + d.x/stepX
-  // console.log(d.y, d.y/stepY, d.x/stepX)
+  const dropRect = this.getBoundingClientRect();
+  const dropPos = [
+    Math.round(dropRect.y / pieceSize[1]),
+    Math.round(dropRect.x / pieceSize[0]) - 2
+  ]
+  const el = d3.select(this)
+  const elData = el.data()[0]
+  const correctPos = elData.pos
+  const correct = correctPos[0] === dropPos[0] && correctPos[1] === dropPos[1]
+  pieceInfo[elData.index].correct = true
 
+  if(isSolved()) {
+    alert('solved')
+  }
+  //debugger;
   d3
     .select(this)
     .raise()
-    	.style("border", "0px");
+    	.style('border', '0px')
+      //.attr('data-correct', correct)
+      //.attr('data-droppedpos', `${dropPos}`)
 }
 
 function translate(d) {
   return `translate(${d.x}px, ${d.y}px)`;
 }
 
+function isSolved() {
+  return pieceInfo.every(d => d.correct || d.hint)
+}
+
 function makePuzzle() {
   imageContainer.selectAll("*").remove();
 
-  const pieceInfo = d3.range(pieceCount).map((d, i) => {
-    return {
-      index: i,
-      hint: Math.random() < hintRatio
-    };
-  });
 
   const hintContainer = imageContainer
     .selectAll(".hints")
@@ -87,48 +93,47 @@ function makePuzzle() {
     (hintPos.x - extraPos.x) % pieceSize[0],
     (hintPos.y - extraPos.y) % pieceSize[1]
   ];
-  console.log(positionDiff);
 
   const hintPieces = hintContainer
     .selectAll(".hint-piece")
     .data(pieceInfo.filter(d => d.hint))
     .enter()
     .append("div")
-    .classed("hint-piece", true)
-    .style("position", "absolute")
-    .style("left", d => (d.index % puzzlePieces[0]) * pieceSize[0] + "px")
-    .style(
-      "top",
-      d => Math.floor(d.index / puzzlePieces[0]) * pieceSize[1] + "px"
-    )
-    .call(setupPiece);
+      .classed("hint-piece", true)
+      .style("position", "absolute")
+      .style("left", d => (d.index % puzzlePieces[0]) * pieceSize[0] + "px")
+      .style(
+        "top",
+        d => Math.floor(d.index / puzzlePieces[0]) * pieceSize[1] + "px"
+      )
+      .call(setupPiece);
 
   const drag = d3
     .drag()
-    .on("start", dragstarted)
-    .on("drag", dragged)
-    .on("end", dragend);
+      .on("start", dragstarted)
+      .on("drag", dragged)
+      .on("end", dragend);
 
   const extraPieces = extraPieceContainer
     .selectAll(".extra")
     .data(d3.shuffle(pieceInfo.filter(d => !d.hint)))
     .enter()
     .append("div")
-    .classed("extra-piece", true)
-    .style("display", "inline-block")
-    .call(setupPiece)
-    .style("transform", (d, i) => {
-      return `translate(${(i % 6) * pieceSize[0]}px, ${Math.floor(i / 6) *
-        pieceSize[1]}px`;
-    })
-    .call(drag);
+      .classed("extra-piece", true)
+      .style("display", "inline-block")
+      .call(setupPiece)
+      .style("transform", (d, i) => {
+        return `translate(${(i % 6) * pieceSize[0]}px, ${Math.floor(i / 6) *
+          pieceSize[1]}px`;
+      })
+      .call(drag);
 }
 
 const imageContainer = d3.select(".artwork-images__images");
 const img = imageContainer.select(
-  ".artwork-images__images__image__display__img"
+  ".artwork-images__images__image__display__img[src]"
 );
-const src = img.attr("src");
+const src = img.attr("src")
 const dim = [img.node().clientWidth, img.node().clientHeight];
 
 const newImage = new Image();
@@ -144,9 +149,20 @@ const pieceCount = puzzlePieces[0] * puzzlePieces[1];
 const hintRatio = 0.4;
 const pieceSize = [dim[0] / puzzlePieces[0], dim[1] / puzzlePieces[1]];
 
+const pieceInfo = d3.range(pieceCount).map((d, i) => {
+  return {
+    index: i,
+    hint: Math.random() < hintRatio,
+    pos: [Math.floor(i / puzzlePieces[0]), i % puzzlePieces[0]]
+  }
+})
+
+console.log(pieceInfo)
+
+
 d3
   .select("body")
   .append("button")
-  .text("make puzzle")
-  .classed("puzzle-btn", true)
-  .on("click", () => makePuzzle());
+    .text("make puzzle")
+    .classed("puzzle-btn", true)
+    .on("click", () => makePuzzle());
