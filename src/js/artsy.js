f08f38de7d849632911d6853582d968d979c25ba
sepans <sepans@sepans.com>
2018-02-12 01:15:27 -0500

    .....

--- src/js/artsy.js
+++ src/js/artsy.js
@@ -35,26 +35,27 @@ function dragend() {
   console.log(this.getBoundingClientRect(), hintPos, pieceSize);
   const dropRect = this.getBoundingClientRect();
   const dropPos = [
-    Math.round(dropRect.y / pieceSize[1]),
-    Math.round(dropRect.x / pieceSize[0]) - 2
+    Math.floor(dropRect.y / pieceSize[1]),
+    Math.floor(dropRect.x / pieceSize[0]) - 2
   ]
   const el = d3.select(this)
   const elData = el.data()[0]
   const correctPos = elData.pos
   const correct = correctPos[0] === dropPos[0] && correctPos[1] === dropPos[1]
-  pieceInfo[elData.index].correct = true
+  pieceInfo[elData.index].correct = correct
+  console.log(dropPos, correctPos, correct)
 
   if(isSolved()) {
     d3.select('.artwork-images').style('height', '65vh')
-    alert('solved')
+    puzzleTimer.classed('solved', true)
+    clearInterval(interval)
+    puzzleBtn.classed('disabled', false)
   }
-  //debugger;
+
   d3
     .select(this)
     .raise()
     	.style('border', '0px')
-      //.attr('data-correct', correct)
-      //.attr('data-droppedpos', `${dropPos}`)
 }
 
 function translate(d) {
@@ -65,11 +66,31 @@ function isSolved() {
   return pieceInfo.every(d => d.correct || d.hint)
 }
 
+function setupTimer() {
+  interval = setInterval(() => {
+    const sec = time % 60
+    const min = Math.floor(time / 60)
+    seconds.text(sec < 10 ? `0${sec}` : sec)
+    minutes.text(min < 10 ? `0${min}` : min)
+    time++
+  }, 1000)
+}
+
 function makePuzzle() {
+
+  pieceInfo = d3.range(pieceCount).map((d, i) => {
+    return {
+      index: i,
+      hint: Math.random() < hintRatio,
+      pos: [Math.floor(i / puzzlePieces[0]), i % puzzlePieces[0]]
+    }
+  })
+    
   d3.select('.artwork-images').style('height', '100vh') //make room for extra pieces
 
   imageContainer.selectAll("*").remove();
 
+  puzzleBtn.classed('disabled', true)
 
   const hintContainer = imageContainer
     .selectAll(".hints")
@@ -86,9 +107,6 @@ function makePuzzle() {
     .enter()
     .append("div")
     .classed("extra", true);
-
-  console.log(hintContainer.node().getBoundingClientRect());
-  console.log(extraPieceContainer.node().getBoundingClientRect());
 
   hintPos = hintContainer.node().getBoundingClientRect();
   extraPos = extraPieceContainer.node().getBoundingClientRect();
@@ -117,7 +135,8 @@ function makePuzzle() {
       .on("drag", dragged)
       .on("end", dragend);
 
-  const hintsPerRow = 8
+  const hintsPerRow = Math.floor(containerSize.width / pieceSize[0])
+  
   const extraPieces = extraPieceContainer
     .selectAll(".extra")
     .data(d3.shuffle(pieceInfo.filter(d => !d.hint)))
@@ -131,9 +150,13 @@ function makePuzzle() {
           pieceSize[1]}px`;
       })
       .call(drag);
+
+  setupTimer()
 }
 
 const imageContainer = d3.select(".artwork-images__images");
+const containerSize = imageContainer.node().getBoundingClientRect()
+console.log('containerSize', containerSize)
 const img = imageContainer.select(
   ".artwork-images__images__image__display__img[src]"
 );
@@ -141,7 +164,7 @@ const src = img.attr("src")
 const dim = [img.node().clientWidth, img.node().clientHeight];
 
 const newImage = new Image();
-let streach, positionDiff, hintPos, extraPos;
+let streach, positionDiff, hintPos, extraPos, pieceInfo
 newImage.src = src;
 newImage.onload = () => {
   const actualSize = [this.width, this.height];
@@ -153,20 +176,30 @@ const pieceCount = puzzlePieces[0] * puzzlePieces[1];
 const hintRatio = 0.4;
 const pieceSize = [dim[0] / puzzlePieces[0], dim[1] / puzzlePieces[1]];
 
-const pieceInfo = d3.range(pieceCount).map((d, i) => {
-  return {
-    index: i,
-    hint: Math.random() < hintRatio,
-    pos: [Math.floor(i / puzzlePieces[0]), i % puzzlePieces[0]]
-  }
-})
+let interval, time = 0
 
-console.log(pieceInfo)
+const puzzleBoard = d3.select("body")
+  .append("div")
+    .classed('puzzle-board', true)
 
-
-d3
-  .select("body")
+const puzzleBtn = puzzleBoard
   .append("button")
     .text("make puzzle")
     .classed("puzzle-btn", true)
     .on("click", () => makePuzzle());
+
+const puzzleTimer = puzzleBoard
+  .append("div")
+    .classed('puzzle-timer', true)
+
+const minutes = puzzleTimer
+  .append("span")
+    .classed('timer-minute', true)
+    .text('00')
+
+const seconds = puzzleTimer
+  .append("span")
+    .classed('timer-second', true)
+    .text('00')
+      
+
